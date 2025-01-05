70e030665f1464049983d4cb37846db77a43a1d5
Aurèle Fontaine <fontaine.aurele@gmail.com>
2025-01-05 01:43:50 +0100

    mise sous Git

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # photobooth
+
+Configuration photobooth à un seul RPi (pas d'affichage à la volée des photos prises via un second écran)
+
+## 1/ Préparation :
+$ sudo apt-get update
+$ sudo apt-get dist-upgrade
+
+Installation de l'économiseur d'écran (pour éviter la veille de l'écran)
+$ sudo apt-get install xscreensaver
+
+en mode graphique, aller dans Modes d'affichage et choisir le mode "Désactiver l'économiseur d'écran"
+
+Librairies python :
+$ sudo apt-get install python-picamera
+$ sudo apt-get install python-RPi.gpio python3-RPi.gpio
+
+## 2/ Câblage des leds et bouton sur le RPi
+Fil rouge : led rouge (BCM 4)
+Fil bleu : led verte (BCM 22)
+Fil jaune : bouton (BCM 17)
+Fil gris : masse
+
+x			x
+x			x
+x			fil gris
+rouge (4)	x
+x			x
+jaune (17)	x
+x			x
+bleu (22)	x
+x			x
+x			x
+x			x
+...h	
+
+## 4/ Post-traitement des images
+Il s'agit de refléter (horizontalement) toutes les images du répertoire
+Installer imagemagick
+--> se placer dans le répertoire concerné (# cd...)
+$ mogrify -flop *.jpg
+
+Remarque sur le post-traitement : si les images sont dans plusieurs répertoires, il est plus pratique de les regrouper dans un unique dossier.
+On les renomme d'après le nom du répertoire
+(nécessite l'installation préalable de rename : # sudo apt-get install rename)
+# rename 's/^/jour-heure/' *
+# mv * ../toutes_les_images
+
+Eventuellement : créer une archive
+# sudo apt-get install zip unzip
+# cd photobooth/img
+# zip -r images_evenement toutes_les_images
+
+
+Remarque : Réglage picamera
+Options de balance des blancs
+'off'
+'auto'
+'sunlight'
+'cloudy'
+'shade'
+'tungsten'
+'fluorescent'
+'incandescent'
+'flash'
+'horizon'
---
+++ photobooth.py
@@ -0,0 +1,129 @@
+########################################################################
+# Module photobooth pour RPi (PiCamera)
+#
+# Les images sont enregistrees en miroir. Pour les retourner
+# utiliser imagemagick et la commande :
+# mogrify -flop *jpg
+#
+########################################################################
+
+from picamera2 import Picamera2, Preview
+from libcamera import Transform
+from RPi import GPIO
+#from tkinter import filedialog
+import tkinter as tk
+import time
+import os
+from PIL import Image, ImageTk
+
+#Dialogue selection du répertoire
+
+root_img_dir = "/home/pi/Documents/photobooth/img/" + time.strftime("%Y-%m-%d_%Hh%M")
+os.makedirs(root_img_dir)
+
+
+def capture_image(camera, preview_config, still_config, root_img_dir, frame):
+    camera.stop()
+    camera.configure(still_config)
+    camera.start()
+    dte_img = time.strftime("%Y-%m-%d_%Hh%M")
+    filename = f"{root_img_dir}/{dte_img}_image{frame:03}.jpg"
+    #filename = '%(dir)s/%(dte)s_image%(frm)03d.jpg' % {"dir": root.directory, "dte": time.strftime("%Y-%m-%d_%Hh%M"), "frm": frame}
+    camera.capture_file(filename)
+    print(f"Image capturée : {filename}")
+    camera.stop()
+    #camera.stop_preview()
+    
+    #display_image(camera, filename, 2)
+        
+    camera.configure(preview_config)
+    camera.start()
+    
+def display_image(camera, filename, duration):
+    image = Image.open(filename)
+    
+    root = tk.Tk()
+    root.attributes('-fullscreen', True)
+    root.configure(background = 'black')
+    
+    tk_image = ImageTk.PhotoImage(image)
+    label = tk.Label(root, image=tk_image, bg='black')
+    label.pack(expand=True)
+    
+    root.after(int(duration * 1000), root.destroy)
+    
+    root.mainloop()
+
+# Config GPIO
+PHOTO_BUTTON = 17
+PHOTO_BUTTON = 17
+STOP_BUTTON = 18
+RED_LED = 4
+GREEN_LED = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PHOTO_BUTTON, GPIO.IN, GPIO.PUD_UP)
+GPIO.add_event_detect(PHOTO_BUTTON, GPIO.RISING, bouncetime=500)
+
+GPIO.setup(STOP_BUTTON, GPIO.IN, GPIO.PUD_UP)
+GPIO.add_event_detect(STOP_BUTTON, GPIO.RISING)
+
+GPIO.setup(RED_LED, GPIO.OUT)
+GPIO.setup(GREEN_LED, GPIO.OUT)
+
+# Config caméra
+camera = Picamera2()
+preview_config = camera.create_preview_configuration(main={"size": (640, 480)}, transform=Transform(vflip=1))
+#preview_config = camera.create_preview_configuration(main={"size": (1920, 1080)}, transform=Transform(vflip=1))
+#still_config = camera.create_still_configuration(main={"size": (1920, 1080)}, transform=Transform(hflip=1, vflip=1))
+#still_config = camera.create_still_configuration(main={"size": (3840, 1920)}, transform=Transform(hflip=1, vflip=1))
+still_config = camera.create_still_configuration(main={"size": (4608, 2592)}, transform=Transform(hflip=1, vflip=1))
+
+# Démarrage caméra
+camera.configure(preview_config)
+camera.start_preview(Preview.QTGL)
+#camera.start_preview(Preview.DRM)
+camera.start()
+
+# Numérotation des photos
+frame = 1
+
+while True:
+
+    GPIO.output(RED_LED, 0)
+    GPIO.output(GREEN_LED, 1)
+    
+    # gestion de l'arrêt par appui sur le bouton dédié
+    if GPIO.event_detected(STOP_BUTTON):
+        camera.stop_preview()
+        camera.close()
+        GPIO.cleanup()
+        print("Photobooth interrompu")
+        break
+
+    if GPIO.event_detected(PHOTO_BUTTON):
+        # lorsque le bouton est actionné, on fait clignoter la led rouge 3s
+        GPIO.output(GREEN_LED, 0)
+        i = 0
+        while i < 2 :
+            GPIO.output(RED_LED, 1)
+            time.sleep(0.5)
+            GPIO.output(RED_LED, 0)
+            time.sleep(0.5)
+            i += 1
+        # puis 2 fois rapidement
+        i = 0
+        while i < 2 :
+            GPIO.output(RED_LED, 1)
+            time.sleep(0.1)
+            GPIO.output(RED_LED, 0)
+            time.sleep(0.1)
+            i += 1
+        
+        # Led rouge allumée, on prend la photo
+        GPIO.output(RED_LED, 1)
+        capture_image(camera, preview_config, still_config, root_img_dir, frame)
+        GPIO.output(RED_LED,0)
+        
+        #incrément du compteur d'images
+        frame += 1
